a24997722fe6e0115d637329c53840f210348785
dammyboss <35613905+dammyboss@users.noreply.github.com>
2022-01-01 13:38:21 +0000

    Update provider.tf

--- prods/prod/provider.tf
+++ prods/prod/provider.tf
@@ -1,13 +1,13 @@
-# # We strongly recommend using the required_providers block to set the
-# # Azure Provider source and version being used
-# terraform {
-#   required_providers {
-#     azurerm = {
-#       source  = "hashicorp/azurerm"
-#       version = "=2.46.0"
-#     }
+# We strongly recommend using the required_providers block to set the
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
     
-#   }
+  }
 //  backend "azurerm" {
 //    resource_group_name  = "TerraformEchonous"
 //    storage_account_name = "terraformechonous"
@@ -16,14 +16,14 @@
 //  }
 }
 
-# Configure the Microsoft Azure Provider
-# provider "azurerm" {
-#   features {}
-#   subscription_id = ""
-# //  client_id       = ""
-# //  client_secret   = var.client_secret
-#   tenant_id       = ""
-# }
+Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = ""
+//  client_id       = ""
+//  client_secret   = var.client_secret
+  tenant_id       = ""
+}
 
 //provider "helm" {
 //  kubernetes {
@@ -35,18 +35,18 @@
 //  }
 //}
 
-###########################
-## Azure Provider - Main ##
-###########################
+# ###########################
+# ## Azure Provider - Main ##
+# ###########################
 
-# Define Terraform provider
-terraform {
-  required_version = ">= 0.12"
-}
+# # Define Terraform provider
+# terraform {
+#   required_version = ">= 0.12"
+# }
 
-# Configure the Azure provider
-provider "azurerm" {
-  environment = "public"
-  version     = ">= 2.15.0"
-  features {}
-}
+# # Configure the Azure provider
+# provider "azurerm" {
+#   environment = "public"
+#   version     = ">= 2.15.0"
+#   features {}
+# }
