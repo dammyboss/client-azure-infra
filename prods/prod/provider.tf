bf0a4fcf4dd7d0bd957eb2f4035fdab0214df8f9
dammyboss <35613905+dammyboss@users.noreply.github.com>
2022-01-01 14:10:14 +0000

    Update provider.tf

--- prods/prod/provider.tf
+++ prods/prod/provider.tf
@@ -19,10 +19,10 @@ terraform {
 Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  subscription_id = ""
-//  client_id       = ""
-//  client_secret   = var.client_secret
-  tenant_id       = ""
+  subscription_id = "85c7908d-8028-4894-af5d-1991ee8f0450"
+  client_id       = "b42b410e-bad5-40e7-af1d-6a224e769ad1"
+  client_secret   = "lg1mt8HEOxiZOSQSoOny4-GxkCH8Mp0KO7"
+  tenant_id       = "37bb0008-2586-4514-a753-d1092d7259f2"
 }
 
 //provider "helm" {
